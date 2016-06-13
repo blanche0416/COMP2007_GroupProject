cbbaf038e659e2bd2ba06ef62a41380af735883a
blanche0416 <blanche0416@gmail.com>
2016-06-13 12:54:32 +0800

    Add team details

--- project1/TeamDetails.aspx
+++ project1/TeamDetails.aspx
@@ -7,4 +7,30 @@
 <%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="TeamDetails.aspx.cs" Inherits="project1.TeamDetails" %>
 
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-3 col-md-6">
+                <h1 class="titleH1">Team Details</h1>
+                <h5>All Fields are Required</h5>
+                <br />
+                <div class="form-group">
+                    <label class="control-label" for="TeamNameTextBox">Team Name</label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="TeamNameTextBox" placeholder="Team Name" required="true"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <label class="control-label" for="ShortDescripTextBox">Short Description</label>
+                    <asp:TextBox runat="server" TextMode="MultiLine" Columns="3" Rows="3" CssClass="form-control" ID="ShortDescripTextBox" placeholder="Short Description" required="true"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <label class="control-label" for="TotalScoreTextBox">Total Point Scored</label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="TotalScoreTextBox" required="true"></asp:TextBox>
+                </div>
+                <div class="text-right">
+                    <asp:Button Text="Cancel" ID="CancelButton" CssClass="btn btn-warning btn-lg" runat="server" UseSubmitBehavior="false" CausesValidation="false" OnClick="CancelButton_Click" />
+                    <asp:Button Text="Save" ID="SaveButton" CssClass="btn btn-primary btn-lg" runat="server" OnClick="SaveButton_Click" />
+                </div>
+            </div>
+        </div>
+    </div>
 </asp:Content>
+
--- project1/TeamDetails.aspx.cs
+++ project1/TeamDetails.aspx.cs
@@ -11,13 +11,95 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using project1.Models;
+using System.Web.ModelBinding;
+
 namespace project1
 {
     public partial class TeamDetails : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if ((!IsPostBack) && (Request.QueryString.Count > 0))
+            {
+                this.GetTeam();
+            }
+        }
+        protected void GetTeam()
+        {
+            //populate teh form with existing data from the database
+            int TeamID = Convert.ToInt32(Request.QueryString["TeamID"]);
 
+            //connect to the EF DB
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                //populate a student object instance with the StudentID from the URL Parameter
+                Team updatedTeam = (from team in db.Teams
+                                    where team.TeamID == TeamID
+                                    select team).FirstOrDefault();
+
+                //map the student properties to the form controls
+                if (updatedTeam != null)
+                {
+                    TeamNameTextBox.Text = updatedTeam.TeamName;
+                    ShortDescripTextBox.Text = updatedTeam.ShortDescription;
+                    TotalScoreTextBox.Text = updatedTeam.TotalPointsScored.ToString();
+                }
+            }
+        }
+        protected void CancelButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/GameTracker.aspx");
+        }
+        protected void SaveButton_Click(object sender, EventArgs e)
+        {
+            //use EF to connect to the server
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                //populate teh form with existing data from the database
+                int TeamID = Convert.ToInt32(Request.QueryString["TeamID"]);
+
+                //populate a student object instance with the StudentID from the URL Parameter
+                Team updatedTeam = (from team in db.Teams
+                                    where team.TeamID == TeamID
+                                    select team).FirstOrDefault();
+                //map the student properties to the form controls
+                if (updatedTeam != null)
+                {
+                    int GameID = updatedTeam.GameID;
+                    int teamScore = Convert.ToInt32(TotalScoreTextBox.Text);
+                    Team anotherTeamScore = (from team in db.Teams
+                                            where team.GameID == GameID && team.TeamID != TeamID
+                                             select team).FirstOrDefault();
+
+                    Game updatedGame = (from game in db.Games
+                                        where game.GameID == GameID
+                                        select game).FirstOrDefault();
+                    if(updatedTeam.TotalPointsScored > anotherTeamScore.TotalPointsScored)
+                    {
+                        updatedGame.DesignatedWinningTeam = updatedTeam.TeamName;
+                    }
+                    else if(updatedTeam.TotalPointsScored < anotherTeamScore.TotalPointsScored)
+                    {
+                        updatedGame.DesignatedWinningTeam = anotherTeamScore.TeamName;
+                    }
+                    else
+                    {
+                        updatedGame.DesignatedWinningTeam = "Draw";
+                    }
+
+                    updatedGame.TotalPointsScored = (teamScore + anotherTeamScore.TotalPointsScored);
+
+                    updatedTeam.TeamName = TeamNameTextBox.Text;
+                    updatedTeam.ShortDescription = ShortDescripTextBox.Text;
+                    updatedTeam.TotalPointsScored = Convert.ToInt32(TotalScoreTextBox.Text);
+                }
+
+                //run an insert command
+                db.SaveChanges();
+                //redirect back to the students page
+                Response.Redirect("~/GameTracker.aspx");
+            }
         }
     }
 }
